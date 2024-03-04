d8b1ed485cd63c6d9802022623519d6089e75c5c
MinJunKimsdaads <aaeexx@naver.com>
2024-03-04 17:44:14 +0900

    20240304

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -35,13 +35,13 @@ export async function fetchRevenue() {
 export async function fetchLatestInvoices() {
   try {
     const data = await sql<LatestInvoiceRaw>`
-      SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
+      SELECT invoices.amount, customers.name, customers.image_url, customers.email
       FROM invoices
       JOIN customers ON invoices.customer_id = customers.id
       ORDER BY invoices.date DESC
       LIMIT 5`;
 
-    const latestInvoices = data.rows.map((invoice) => ({
+    const latestInvoices = data.rows.map((invoice:any) => ({
       ...invoice,
       amount: formatCurrency(invoice.amount),
     }));
