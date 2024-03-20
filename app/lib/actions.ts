17c4eae73ab8c1d3ae9f2764344a304f95f01d12
MinJunKimsdaads <aaeexx@naver.com>
2024-03-20 17:20:58 +0900

    20240320

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,10 +1,11 @@
 'use server';
 
-import { z } from 'zod';
+import {z} from "Zod";
 import { sql } from '@vercel/postgres';
 import { revalidatePath } from '@/node_modules/next/cache';
 import { redirect } from '@/node_modules/next/navigation';
 
+//zod//
 const FormSchema = z.object({
     id: z.string(),
     customerId: z.string(),
@@ -14,6 +15,8 @@ const FormSchema = z.object({
 });
 
 const CreateInvoice = FormSchema.omit({id:true,date:true});
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
+//zod//
 
 export async function createInvoice(formData:FormData){
 
@@ -31,7 +34,34 @@ export async function createInvoice(formData:FormData){
         VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
     `;
 
+    revalidatePath('/dashboard/invoices'); //라우터에 캐시를 삭제
+    redirect('/dashboard/invoices'); //그후에 리디렉션
+    
+}
+
+export async function updateInvoice(id: string, formData: FormData) {
+    // console.log(id);
+    // console.log(formData);
+
+    const { customerId, amount, status } = UpdateInvoice.parse({
+        customerId: formData.get('customerId'),
+        amount: formData.get('amount'),
+        status: formData.get('status'),
+    });
+   
+    const amountInCents = amount * 100;
+   
+    await sql`
+      UPDATE invoices
+      SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+      WHERE id = ${id}
+    `;
+   
     revalidatePath('/dashboard/invoices');
     redirect('/dashboard/invoices');
-    
+  }
+
+export async function deleteInvoice(id: string) {
+    await sql`DELETE FROM invoices WHERE id = ${id}`;
+    revalidatePath('/dashboard/invoices');
 }
